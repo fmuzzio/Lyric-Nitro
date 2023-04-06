c0b18055e30578c67a8bfe089c2070d0a470d4e4
fmuzzio <fmuzzio@seas.upenn.edu>
2023-04-06 12:50:51 -0700

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # LyricNitro
+
+# Text Generator
+
+A Python project that generates text using an Object Oriented architecture with TensorFlow and Keras.
+
+## Getting Started
+
+1. Clone this repository.
+2. Install the required packages: `pip install -r requirements.txt`
+3. Run `main.py`: `python main.py`
+
+## Usage
+
+You can train the model and generate text using the `TextGenerator` class in `src/text_generator.py`. Modify the `main.py` script to use your own data and configurations.
---
+++ src/textgenerator.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+class TextGenerator:
+    def __init__(self, input_file):
+        self.data = open(input_file, 'r').read()
+        self.tokenizer = Tokenizer()
+        self.corpus = self.data.lower().split("\n")
+        self.tokenizer.fit_on_texts(self.corpus)
+        self.total_words = len(self.tokenizer.word_index) + 1
+        self.input_sequences = self.prepare_input_sequences()
+        self.max_sequence_len = max([len(x) for x in self.input_sequences])
+        self.input_sequences = np.array(pad_sequences(self.input_sequences, maxlen=self.max_sequence_len, padding='pre'))
+        self.xs, self.ys = self.prepare_data()
+        self.model = self.build_model()
+
+    def prepare_input_sequences(self):
+        input_sequences = []
+        for line in self.corpus:
+            token_list = self.tokenizer.texts_to_sequences([line])[0]
+            for i in range(1, len(token_list)):
+                n_gram_sequence = token_list[:i + 1]
+                input_sequences.append(n_gram_sequence)
+        return input_sequences
+
+    def prepare_data(self):
+        xs = self.input_sequences[:, :-1]
+        labels = self.input_sequences[:, -1]
+        ys = tf.keras.utils.to_categorical(labels, num_classes=self.total_words)
+        return xs, ys
+
+    def build_model(self):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Embedding(self.total_words, 64, input_length=self.max_sequence_len - 1),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150)),
+            tf.keras.layers.Dense(self.total_words, activation='softmax')
+        ])
+
+        model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=0.01), metrics=['accuracy'])
+        return model
+
+
+    def train(self, epochs=100, verbose=1):
+        self.model.fit(self.xs, self.ys, epochs=epochs, verbose=verbose)
+
+
+    def generate_text(self, seed_text, next_words=100):
+        for _ in range(next_words):
+            token_list = self.tokenizer.texts_to_sequences([seed_text])[0]
+            token_list = pad_sequences([token_list], maxlen=self.max_sequence_len - 1, padding='pre')
+            predicted = self.model.predict_classes(token_list, verbose=0)
+            output_word = ""
+    
+            for word, index in self.tokenizer.word_index.items():
+
+                if index == predicted:
+                    output_word = word
+                    break
+            seed_text += " " + output_word
+        return seed_text
+
